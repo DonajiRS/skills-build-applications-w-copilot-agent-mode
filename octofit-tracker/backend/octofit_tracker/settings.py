ae84afcabdb8217218910d3fbe93d34e2ef81020
Jonathan Donaji Rangel Sánchez <127538180+DonajiRS@users.noreply.github.com>
2025-04-09 20:20:17 +0000

    feat: Reorganizar configuración de base de datos MongoDB en settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -15,6 +15,19 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+# Add MongoDB connection
+DATABASES = {
+    "default": {
+        "ENGINE": "djongo",
+        "NAME": "octofit_db",
+        "PORT": 27017,
+        "HOST": "localhost",
+    }
+}
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -74,19 +87,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# Add MongoDB connection
-DATABASES = {
-    "default": {
-        "ENGINE": "djongo",
-        "NAME": "octofit_db",
-        "PORT": 27017,
-        "HOST": "localhost",
-    }
-}
 
 
 # Password validation
